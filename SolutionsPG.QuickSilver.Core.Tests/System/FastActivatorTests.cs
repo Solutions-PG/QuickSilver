502d3b4f0722c4a34ac08c7ee9f002d80d55e93b
pasgrav <pasgrav@users.noreply.github.com>
2017-08-27 02:32:52 -0400

    Updated the generic FastActivator to manage most constructors, up to 3 parameters, while keeping near-"direct call through lambda" level of performance, not bad.

---
+++ SolutionsPG.QuickSilver.Core.Tests/System/FastActivatorTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SolutionsPG.QuickSilver.Core.System;
+using System.Text;
+
+
+namespace SolutionsPG.QuickSilver.Core.Tests.System
+{
+    [TestClass()]
+    public class FastActivatorTests
+    {
+        [TestMethod, TestCategory("_Unit"), TestCategory("Core.System"), TestCategory("Core.System.FastActivator")]
+        public void System_FastActivator_CreateInstante_Class_ReturnValidConstructor()
+        {
+            //Arrange
+
+            //Act
+            StringBuilder result = FastActivator<StringBuilder>.CreateInstance();
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod, TestCategory("_Unit"), TestCategory("Core.System"), TestCategory("Core.System.FastActivator")]
+        public void System_FastActivator_CreateInstante_Class_1Arg_ReturnValidConstructor()
+        {
+            //Arrange
+            int capacity = 256;
+
+            //Act
+            StringBuilder result = FastActivator<int, StringBuilder>.CreateInstance(capacity);
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(capacity, result.Capacity);
+        }
+        [TestMethod, TestCategory("_Unit"), TestCategory("Core.System"), TestCategory("Core.System.FastActivator")]
+        public void System_FastActivator_CreateInstante_Struct_ReturnValidConstructor()
+        {
+            //Arrange
+
+            //Act
+            TestStruct result = FastActivator<TestStruct>.CreateInstance();
+
+            //Assert
+        }
+
+        [TestMethod, TestCategory("_Unit"), TestCategory("Core.System"), TestCategory("Core.System.FastActivator")]
+        public void System_FastActivator_CreateInstante_Struct_1Arg_ReturnValidConstructor()
+        {
+            //Arrange
+            int value = 4;
+
+            //Act
+            TestStruct2 result = FastActivator<int, TestStruct2>.CreateInstance(value);
+
+            //Assert
+            Assert.AreEqual(value, result.TestInt);
+        }
+
+        struct TestStruct { }
+
+        struct TestStruct2
+        {
+            public int TestInt { get; }
+
+            public TestStruct2(int testInt)
+            {
+                TestInt = testInt;
+            }
+        }
+    }
+}
--- SolutionsPG.QuickSilver.Core/System/FastActivator.cs
+++ SolutionsPG.QuickSilver.Core/System/FastActivator.cs
@@ -1,39 +1,82 @@
 ﻿using System;
-using System.Linq.Expressions;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 
 namespace SolutionsPG.QuickSilver.Core.System
 {
-    public static class FastActivator<T> where T : new()
+    public static class FastActivator<TResult>
     {
-        public static readonly Func<T> CreateInstance = FastConstructorGenerator.Generate<T>();
+        public static readonly Func<TResult> CreateInstance = FastConstructorGenerator.Generate<TResult>();
+        public static Func<object[], object> GetConstructor(IEnumerable<Type> arguments)
+        {
+            return GetConstructor(arguments.ToArray());
+        }
+        public static Func<object[], object> GetConstructor(params Type[] arguments)
+        {
+            var constructorInfo = TypeCache<TResult>.Type.GetConstructor(arguments);
+            return (constructorInfo != null) ? constructorInfo.Invoke : (Func<object[], object>)null; ;
+        }
     }
 
-    public static class FastConstructorGenerator
+    public static class FastActivator<T, TResult>
     {
-        private static class Params
+        public static readonly Func<T, TResult> CreateInstance = FastConstructorGenerator.Generate<T, TResult>();
+    }
+
+    public static class FastActivator<T1, T2, TResult>
+    {
+        public static readonly Func<T1, T2, TResult> CreateInstance = FastConstructorGenerator.Generate<T1, T2, TResult>();
+    }
+
+    public static class FastActivator<T1, T2, T3, TResult>
+    {
+        public static readonly Func<T1, T2, T3, TResult> CreateInstance = FastConstructorGenerator.Generate<T1, T2, T3, TResult>();
+    }
+
+    internal static class FastConstructorGenerator
+    {
+        private static readonly Random Random = new Random();
+        private const bool SkipVisibility = true;
+        private const string MethodNameSuffix = "_GeneratedByFastConstructor";
+
+        public static Func<TResult> Generate<TResult>()
         {
-            public const string Name = "GeneratedFastConstructor";
-            public static readonly Type[] ParameterTypes = new Type[0];
-            public static readonly Module Module = typeof(FastConstructorGenerator).Module;
-            public const bool SkipVisibility = true;
+            return (Func<TResult>)Generate_(TypeCache<TResult>.Type, TypeCache<Func<TResult>>.Type);
         }
 
-        public static Func<T> Generate<T>() where T : new()
+        public static Func<T, TResult> Generate<T, TResult>()
         {
-            Expression<Func<T>> constructorRawDefinition = () => new T();
-            var constructorDefinition = (NewExpression)constructorRawDefinition.Body;
-            var constructorType = constructorDefinition.Type;
+            return (Func<T, TResult>)Generate_(TypeCache<TResult>.Type, TypeCache<Func<T, TResult>>.Type, TypeCache<T>.Type);
+        }
 
-            var methodInfo = new DynamicMethod(Params.Name, constructorType, Params.ParameterTypes,
-                Params.Module, Params.SkipVisibility);
+        public static Func<T1, T2, TResult> Generate<T1, T2, TResult>()
+        {
+            return (Func<T1, T2, TResult>)Generate_(TypeCache<TResult>.Type, TypeCache<Func<T1, T2, TResult>>.Type, TypeCache<T1>.Type, TypeCache<T2>.Type);
+        }
 
-            var ilGenerator = methodInfo.GetILGenerator();
+        public static Func<T1, T2, T3, TResult> Generate<T1, T2, T3, TResult>()
+        {
+            return (Func<T1, T2, T3, TResult>)Generate_(TypeCache<TResult>.Type, TypeCache<Func<T1, T2, T3, TResult>>.Type, TypeCache<T1>.Type, TypeCache<T2>.Type, TypeCache<T3>.Type);
+        }
 
-            var constructorInfo = constructorDefinition.Constructor;
+        private static Delegate Generate_(Type constructorType, Type delegateType, params Type[] parameterTypes)
+        {
+            var constructorInfo = constructorType.GetConstructor(parameterTypes);
+            if (constructorInfo == null && (constructorType.IsValueType == false || parameterTypes.Length > 0))
+            {
+                return null;
+            }
+            //-----
+
+            var methodInfo = new DynamicMethod(constructorType.Name + Random.Next() + MethodNameSuffix,
+                constructorType, parameterTypes, constructorType.Module, SkipVisibility);
+
+            ILGenerator ilGenerator = methodInfo.GetILGenerator();
             if (constructorInfo != null)
             {
+                ilGenerator.EmitArgs(parameterTypes.Length);
                 ilGenerator.Emit(OpCodes.Newobj, constructorInfo);
             }
             else
@@ -44,10 +87,35 @@ namespace SolutionsPG.QuickSilver.Core.System
                 ilGenerator.Emit(OpCodes.Initobj, constructorType);
                 ilGenerator.Emit(OpCodes.Ldloc, builder);
             }
-
             ilGenerator.Emit(OpCodes.Ret);
 
-            return (Func<T>)methodInfo.CreateDelegate(typeof(Func<T>));
+            return methodInfo.CreateDelegate(delegateType);
+        }
+
+        private static void EmitArgs(this ILGenerator ilGenerator, int nbArgs)
+        {
+            if (nbArgs >= 1)
+            {
+                ilGenerator.Emit(OpCodes.Ldarg_0);
+                if (nbArgs >= 2)
+                {
+                    ilGenerator.Emit(OpCodes.Ldarg_1);
+                    if (nbArgs >= 3)
+                    {
+                        ilGenerator.Emit(OpCodes.Ldarg_2);
+                        if (nbArgs >= 4)
+                        {
+                            ilGenerator.Emit(OpCodes.Ldarg_3);
+
+                            nbArgs -= 4;
+                            for (byte i = 4; nbArgs > 0; --nbArgs, ++i)
+                            {
+                                ilGenerator.Emit(OpCodes.Ldarg_S, i);
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
